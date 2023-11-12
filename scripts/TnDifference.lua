d39e152ecab05f1ff4a33dd80be3f683347b8634
Ivan Kostruba <37800708+IvanKostruba@users.noreply.github.com>
2023-11-12 15:44:12 +0100

    Initial commit

---
+++ scripts/TnDifference.lua
@@ -0,0 +1,158 @@
+function registerOptions()
+	local numbers = "option_val_01|option_val_02|option_val_03|option_val_04|option_val_05|option_val_06|option_val_07|option_val_08|option_val_09|option_val_10"
+	local zeroAndNumbers = "option_val_00|option_val_01|option_val_02|option_val_03|option_val_04|option_val_05|option_val_06|option_val_07|option_val_08|option_val_09|option_val_10"
+	OptionsManager.registerOption2(
+		"TND_show_attack_over", false, "option_header_TND", "option_label_TND_attacks_over", "option_entry_cycler",
+		{labels=zeroAndNumbers, values="0|1|2|3|4|5|6|7|8|9|10", baselabel="option_val_none", baseval="none", default="5"}
+	)
+	OptionsManager.registerOption2(
+		"TND_show_attack_under", false, "option_header_TND", "option_label_TND_attacks_under", "option_entry_cycler",
+		{labels=numbers, values ="1|2|3|4|5|6|7|8|9|10", baselabel="option_val_none", baseval="none", default="none"}
+	)
+	OptionsManager.registerOption2(
+		"TND_show_saves_over", false, "option_header_TND", "option_label_TND_saves_over", "option_entry_cycler",
+		{labels=zeroAndNumbers, values ="0|1|2|3|4|5|6|7|8|9|10", baselabel="option_val_none", baseval="none", default="none"}
+	)
+	OptionsManager.registerOption2(
+		"TND_show_saves_under", false, "option_header_TND", "option_label_TND_saves_under", "option_entry_cycler",
+		{labels=numbers, values="1|2|3|4|5|6|7|8|9|10", baselabel="option_val_none", baseval="none", default="5"}
+	)
+	OptionsManager.registerOption2(
+		"TND_show_skills_over", false, "option_header_TND", "option_label_TND_skills_over", "option_entry_cycler",
+		{labels=zeroAndNumbers, values ="0|1|2|3|4|5|6|7|8|9|10", baselabel="option_val_none", baseval="none", default="none"}
+	)
+	OptionsManager.registerOption2(
+		"TND_show_skills_under", false, "option_header_TND", "option_label_TND_skills_under", "option_entry_cycler",
+		{labels=numbers, values ="1|2|3|4|5|6|7|8|9|10", baselabel="option_val_none", baseval="none", default="none"}
+	)
+end
+
+function onInit()
+	registerOptions()
+
+	ActionAttack.applyAttackTNDOrig = ActionAttack.applyAttack
+	ActionAttack.applyAttack = applyAttackDecorator
+
+	ActionSave.applySaveTNDOrig = ActionSave.applySave
+	ActionSave.applySave = applySaveDecorator
+
+	ActionCheck.onRollTNDOrig = ActionCheck.onRoll
+	ActionCheck.onRoll = checkRollDecorator
+	-- have to re-register since we replaced the registered function
+	ActionsManager.registerResultHandler("check", ActionCheck.onRoll)
+
+	ActionSkill.onRollTNDOrig = ActionSkill.onRoll
+	ActionSkill.onRoll = skillRollDecorator
+	ActionsManager.registerResultHandler("skill", ActionSkill.onRoll)
+end
+
+function onPreAttackDecorator(rSource, rTarget, rRoll, rMessage)
+	-- Call the original onPreAttackResolve function
+	ActionAttack.onPreAttackResolveTNDOrig(rSource, rTarget, rRoll, rMessage)
+	if rRoll.nDefenseVal == nil then
+		return
+	end
+	setTnDiffState(rSource, rTarget, rRoll.nTotal - rRoll.nDefenseVal)
+end
+
+function highlightAttacks()
+	return not (OptionsManager.isOption("TND_show_attack_over", "none")
+		and OptionsManager.isOption("TND_show_attack_under", "none"))
+end
+
+function highlightThreshold(sOpt)
+	local sVal = OptionsManager.getOption(sOpt)
+	if sVal ~= "none" then
+		return tonumber(sVal)
+	end
+end
+
+function prepareChatMessages(nDiff, sOverOpt, sUnderOpt, sTargetName)
+	local text = ""
+	local icon = ""
+	overTreshold = highlightThreshold(sOverOpt)
+	underThreshold = highlightThreshold(sUnderOpt)
+	if overTreshold and nDiff >= overTreshold then
+		text = string.format("OVER %s BY [%d] OR MORE", sTargetName, overTreshold)
+		icon = "over_tn"
+	elseif underThreshold and nDiff <= -underThreshold then
+		text = string.format("UNDER %s BY [%d] OR MORE", sTargetName, underThreshold)
+		icon = "under_tn"
+	end
+	if text == "" then return nil, nil end
+	local msgShort = { font = "msgfont", ["text"] = text, ["icon"] = icon}
+	local msgLong = { font = "msgfont", ["text"] = text, ["icon"] = icon}
+	-- typically the short one is for players and the long one is fo DM
+	-- but for now they are identical.
+	return msgShort, msgLong
+end
+
+function applyAttackDecorator(rSource, rTarget, rRoll)
+	-- call the original applyAttack method
+	ActionAttack.applyAttackTNDOrig(rSource, rTarget, rRoll)
+	-- rRoll named after the same parameter in ActionAttack,
+	-- but it is actually the action itself, not a die roll data.
+	local nDiff
+	if rRoll.nDefenseVal ~= nil then
+		nDiff = rRoll.nTotal - rRoll.nDefenseVal
+	end
+	if highlightAttacks() and nDiff ~= nil then
+		local msgShort, msgLong = prepareChatMessages(
+			nDiff, "TND_show_attack_over", "TND_show_attack_under", "AC")
+		if msgShort ~= nil and msgLong ~= nil then
+			ActionsManager.outputResult(rRoll.bSecret, rSource, rTarget, msgLong, msgShort)
+		end
+	end
+end
+
+function highlightSaves()
+	return not (OptionsManager.isOption("TND_show_saves_over", "none")
+		and OptionsManager.isOption("TND_show_saves_under", "none"))
+end
+
+function applySaveDecorator(rSource, rOrigin, rAction, sUser)
+	-- call the original applySave method
+	ActionSave.applySaveTNDOrig(rSource, rOrigin, rAction, sUser)
+
+	-- saves handler replaces nil with 0
+	if highlightSaves() and rAction.nTarget ~= 0 then
+		local msgShort, msgLong = prepareChatMessages(
+			rAction.nTotal - rAction.nTarget, "TND_show_saves_over", "TND_show_saves_under", "DC")
+		if msgShort ~= nil and msgLong ~= nil then
+			ActionsManager.outputResult(rAction.bSecret, rSource, rOrigin, msgLong, msgShort)
+		end
+	end
+end
+
+function highlightSkills()
+	return not (OptionsManager.isOption("TND_show_skills_over", "none")
+		and OptionsManager.isOption("TND_show_skills_under", "none"))
+end
+
+function checkRollDecorator(rSource, rTarget, rRoll)
+	-- call the original onRoll method
+	ActionCheck.onRollTNDOrig(rSource, rTarget, rRoll)
+
+	if highlightSkills() and rRoll.nTarget ~= nil and rRoll.nTarget ~= 0 then
+		local rollTotal = ActionsManager.total(rRoll);
+		local msgShort, _ = prepareChatMessages(
+			rollTotal - rRoll.nTarget, "TND_show_skills_over", "TND_show_skills_under", "DC")
+		if msgShort ~= nil then
+			Comm.deliverChatMessage(msgShort)
+		end
+	end
+end
+
+function skillRollDecorator(rSource, rTarget, rRoll)
+	-- call the original onRoll method
+	ActionSkill.onRollTNDOrig(rSource, rTarget, rRoll)
+
+	if highlightSkills() and rRoll.nTarget ~= nil and rRoll.nTarget ~= 0 then
+		local rollTotal = ActionsManager.total(rRoll);
+		local msgShort, _ = prepareChatMessages(
+			rollTotal - rRoll.nTarget, "TND_show_skills_over", "TND_show_skills_under", "DC")
+		if msgShort ~= nil then
+			Comm.deliverChatMessage(msgShort)
+		end
+	end
+end
